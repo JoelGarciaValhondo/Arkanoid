e2d2220bcfc18c81cb7c8574f2723f3dcf4b86e6
Joel Garcia Valhondo <joel.garcia.wk@gmail.com>
2021-11-23 19:24:25 +0100

    commit inicial del proyecto Arkanoid. La bola rebota casi bien los ladrillos desaparecen faltan las imagenes falta que el cursor funcione bien

---
+++ src/codigo/Cursor.java
@@ -0,0 +1,22 @@
+package codigo;
+
+import java.awt.Color;
+
+import acm.graphics.GRect;
+
+public class Cursor extends GRect{
+
+	public Cursor(double x, double y, double width, double height, Color c) {
+		super(x, y, width, height);
+		setFilled(true);
+		setFillColor(c);
+	}
+	
+	public void muevete(int anchoPantalla, int posX){
+		if(posX + getWidth() < anchoPantalla){
+			setLocation(posX, getY());
+		}	
+	}
+
+}
+
